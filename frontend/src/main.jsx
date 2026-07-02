bcbb74454382f1db80f0d3877a68e4710935bc0d
tiinkuq <tiinkuq@gmail.com>
2026-07-02 09:53:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,20 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/App.css';
 
-// ✅ Add this to fix hydration
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+// Simple render
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
